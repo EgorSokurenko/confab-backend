63f8e44fb0f68c3d4d1d8b6f8da87dbb5006d75f
Egor Sokurenko <sokurenko337@gmail.com>
2023-02-25 12:44:43 +0200

    maybe

--- middlewares/pdfManager.js
+++ middlewares/pdfManager.js
@@ -1,30 +1,22 @@
 'use strict'
-const fs = require('fs'),
-  Path = require('path'),
-  Handlebars = require('handlebars'),
-  puppeteer = require('puppeteer')
 
+const puppeteer = require('puppeteer')
 
 module.exports = {
   pdf: async html => {
-
-    try {
-      const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-      })
-      const page = await browser.newPage()
-      await page.setContent(html, {
-        waitUntil: 'networkidle0'
-      })
-      const buffer = await page.pdf({
-        printBackground: true,
-        format: 'Letter'
-      })
-      await page.close()
-      await browser.close()
-      return buffer
-    } catch (err) {
-      throw err
-    }
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+    })
+    const page = await browser.newPage()
+    await page.setContent(html, {
+      waitUntil: 'networkidle0'
+    })
+    const buffer = await page.pdf({
+      printBackground: true,
+      format: 'Letter'
+    })
+    await page.close()
+    await browser.close()
+    return buffer
   }
 }
